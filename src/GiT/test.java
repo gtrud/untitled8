a664663208d3ea6f711636ceb52f385bb2e8bcdb
hubo <3555167833@qq.com>
2023-02-27 14:04:25 +0800

    hotfix first commit

--- src/GiT/test.java
+++ src/GiT/test.java
@@ -5,5 +5,6 @@ public class test {
         System.out.println("hello world");
         System.out.println("hello git");
         System.out.println("hello world");
+        System.out.println("hello hotfix");
     }
 }
